176459b8f757534a7bcd13922c27dd97f7e1a673
Anima Yadav <animaydv800@gmail.com>
2024-01-24 17:56:18 +0545

    js tutorials

---
+++ index.js
@@ -0,0 +1,77 @@
+//prints the message
+console.log("hello world");
+console.log("anima yadav");
+
+//variables
+//var(ES5)
+
+var a ="anima";
+console.log(a);
+var broadwayTeacher =" bibek"; 
+broadwayTeacher ="shyam";
+//always use camelCase for joining two or more words for variables
+console.log(broadwayTeacher);
+
+//let , const(ES6)
+
+let teacher ="anima";
+teacher = "bibek";
+console.log(teacher);
+
+const teach ="bibek";
+console.log(teach);
+
+/*
+ create 4 variables using table,fan,
+ chair,pen (multiline comment)
+ */
+//const
+
+const tableRound ="ikea";
+const fanColor ="toofan";
+const studentChair ="furniture";
+const redpen =" camel pen";
+console.log(tableRound,fanColor,studentChair,redpen);
+
+console.log(5+11);
+
+
+//use var and let keyword for 4 variables
+
+//var(ES5)
+
+var name1 = "anima yadav";
+name1 = "bibek sharma";
+var age1 = 22;
+age1 = 25;
+var address = "tinkune";
+address = "balkumari";
+var college ="new summit";
+college = "national infotech";
+console.log(name1,age1,address,college);
+
+//let(ES6)
+
+let teacherAge = 30;
+let institute = "broadway";
+let course = "javascript";
+let courseFee = 25000;
+teacherAge = 20;
+institute = "Bi";
+course = "js";
+courseFee = "pencil";
+console.log(teacherAge,institute,course,courseFee);
+
+//const(ES6)
+
+const myAge = 22;
+const myPhone = 98098000;
+const email = "animaydv800@gmail.com";
+const myCourse = "mern stack";
+console.log(myAge,myPhone,email,myCourse);
+
+
+//interactions
+// prompt,alert,confirm
+
+const
