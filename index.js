2c16a91edacc246335c4e09c088901d3c4604a08
Anima Yadav <animaydv800@gmail.com>
2024-01-25 09:14:08 +0545

    practice

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+/*
 //prints the message
 console.log("hello world");
 console.log("anima yadav");
@@ -20,13 +22,14 @@ console.log(teacher);
 
 const teach ="bibek";
 console.log(teach);
+*/
 
 /*
  create 4 variables using table,fan,
  chair,pen (multiline comment)
  */
 //const
-
+/*
 const tableRound ="ikea";
 const fanColor ="toofan";
 const studentChair ="furniture";
@@ -34,12 +37,13 @@ const redpen =" camel pen";
 console.log(tableRound,fanColor,studentChair,redpen);
 
 console.log(5+11);
+*/
 
 
 //use var and let keyword for 4 variables
 
 //var(ES5)
-
+/*
 var name1 = "anima yadav";
 name1 = "bibek sharma";
 var age1 = 22;
@@ -69,9 +73,76 @@ const myPhone = 98098000;
 const email = "animaydv800@gmail.com";
 const myCourse = "mern stack";
 console.log(myAge,myPhone,email,myCourse);
-
+*/
 
 //interactions
 // prompt,alert,confirm
+/*
 
-const
+const isAdult = confirm("are you adult");
+console.log(isAdult);
+
+const name = prompt("what is your name");
+console.log(name);
+
+alert("hello from anima yadav");
+*/
+
+
+//data types
+
+//const pi =3.14;
+
+
+
+/*
+const gender = prompt("what is your gender? m/f");
+
+// if(gender==="m") {
+//     alert("you are male");
+// } else {
+//     alert("you are female")
+// }
+*/
+
+/*
+switch(gender){
+case "m":
+    alert("you are male");
+    break;
+    case "f":
+        alert("you are female");
+        break;
+        default:
+        alert("invalid input.try again");
+}
+*/
+
+
+//write a program that welcomes user based on their name and password input.
+ /*
+const username = "animayadav";
+const password = "animayadav";
+
+if(username===password){
+    alert("welcome anima yadav");
+}else{
+   alert("try again.invalid input");
+}
+*/
+
+ /*
+const userName = prompt("what is your username");
+const password1 = prompt("enter password");
+
+if(userName===password1){
+    alert(`welcome ${userName}`); //string literal
+}else{
+    alert("try again.enter valid input");
+    
+}
+*/
+
+
+//write a program that asks user for
+// addition  of 2 numbers and alert the result
